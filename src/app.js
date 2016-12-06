1e53cc1aaba101e18e6ce00fea58fdacecaab81b
nexusA <dongkyu91622@gmail.com>
2016-12-07 02:28:59 +0900

    두번재 commit

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 const express = require('express');
-let app = express();
+const app = express();
+
+app.use(express.static('public'));
+app.locals.pretty = true;
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>404 Not Found</h1>');
+});
+
+app.listen(3000, () => {
+});
