1f28f9a8257d4527a6f0272c662ac3c7d8a14091
nexusA <dongkyu91622@gmail.com>
2016-12-09 13:06:18 +0900

    background-color, import css File fix and... many bugs!

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
 
+
 app.use(express.static('public'));
 app.locals.pretty = true;
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('main', {
+        
+        title: "MyWeb",
+        program: "Node.js"
+    });
+
+    });
+
 
 app.all('*', (req, res) => {
     res.status(404).send('<h1>404 Not Found</h1>');
 });
 
 app.listen(3000, () => {
+    console.log("loading...");
 });
